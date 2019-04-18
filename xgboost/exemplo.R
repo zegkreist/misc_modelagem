c78d3cdea23975829a373f085ee9ede5c71a4468
nrosa <nrosa@HEAL0012.funcional.local>
2019-04-18 14:36:24 -0300

    align

--- xgboost/exemplo.R
+++ xgboost/exemplo.R
@@ -20,27 +20,27 @@ source("predict_ensemble_xgboost.R")
 source("selecao_parametros_xgboost.R")
 source("treino_ensemble_xgboost.R")
 
-parametros <- xgb_select_params(dados = dados,
+parametros <- xgb_select_params(dados     = dados,
                                 positivos = 20,
-                                objetivo = "multi:softmax",
-                                metrica = "mlogloss",
-                                niter = 10
+                                objetivo  = "multi:softmax",
+                                metrica   = "mlogloss",
+                                niter     = 10
                                 )
 
 
 
-xgb_treino_ensemble(dados = dados,
-                    positivos = 10, 
-                    parametros = parametros,
+xgb_treino_ensemble(dados           = dados,
+                    positivos       = 10, 
+                    parametros      = parametros,
                     save_importance = T,
-                    n_models = 100,
-                    folder_to_save = "teste_models"
+                    n_models        = 100,
+                    folder_to_save  = "teste_models"
                     )
 
 
-predito_final <- xgb_predict_ensemble(folder = "teste_models",
+predito_final <- xgb_predict_ensemble(folder  = "teste_models",
                                       newdata = dados,
-                                      prob = F
+                                      prob    = F
                                       )
 
 
